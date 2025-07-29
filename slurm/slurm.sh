613754ca2aefb43efb12ee8f13a10ab728467d20
Maria J. Sanchez Rincon <mariajsanchez006@gmail.com>
2025-07-29 12:46:24 -0700

    Update slurm.sh

--- slurm/slurm.sh
+++ slurm/slurm.sh
@@ -15,7 +15,7 @@ echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 
 
 module load python
-conda activate /Users/mariasanchezrincon/anaconda3/envs/emcee_fitting
+conda activate /Users/mariasanchezrincon/anaconda3/envs/conda_emcee_fitting
 
 python3 O3_1comp.py $SLURM_ARRAY_TASK_ID
 Collapse
